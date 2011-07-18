c1d98f4f1190ef997a25101d6c45e03de6af1bf0
Ved <ved.antani@gmail.com>
2011-07-18 11:20:57 +0530

    null check in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,15 +7,21 @@ module ApplicationHelper
   def google_analytics_id
     ENV['GOOGLE_ANALYTICS_ID']
   end
+
+
   def dots (v)
-    if v.length>17 then
+    
+    if v ==nil
+      return
+    end
+      
+    if v.length > 17 then
         v[0..17] + "..."
     else
         v
     end        
+
+
+
   end
-  
-
-
-
 end
