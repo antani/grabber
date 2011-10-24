cc08cd745d8b683427b19e4e9d98c743f91f9362
Ved <antani@Ved.(none)>
2011-10-24 18:07:12 +0530

    moved back to rvm based capistrano recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # RVM
 
-#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-#require "rvm/capistrano"
-#set :rvm_bin_path, "/usr/local/bin"
-#set :rvm_ruby_string, 'default'
-#set :rvm_type, :user
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_bin_path, "/usr/local/bin"
+set :rvm_ruby_string, 'default'
+set :rvm_type, :user
 
-set :default_environment, {
-  'PATH' => "/root/.rbenv/shims:/root/.rbenv/bin:$PATH"
-}
+#set :default_environment, {
+#  'PATH' => "/root/.rbenv/shims:/root/.rbenv/bin:$PATH"
+#}
 
 
 # Bundler
