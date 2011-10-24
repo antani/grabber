56f27a51b35388b123a75558ea34e46912de5c7e
Ved <antani@Ved.(none)>
2011-10-24 17:59:00 +0530

    fixed rvm and mongoid issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
+require 'yaml'
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+YAML::ENGINE.yamler= 'syck'
 Encoding.default_internal = 'UTF-8'
 # Initialize the rails application
 Isbnnetin::Application.initialize!
