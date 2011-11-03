8c33ace35b0a477205390f7348f385c616e7662b
Ved <ved.antani@gmail.com>
2011-11-03 18:13:18 +0530

    jammit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -243,7 +243,7 @@ function dots(v){
 $('.comingsoon').qtip({content: 'Coming very very soon', show:'mouseover',hide:'mouseout',style:'green'});
 
 /*--Feedback slidshow----------------*/
-$('#slideshow-reel').innerfade({animationtype: 'slide', speed: 750, timeout: 8000, type: 'random', containerheight: '1em' }); 
+//$('#slideshow-reel').innerfade({animationtype: 'slide', speed: 750, timeout: 8000, type: 'random', containerheight: '1em' }); 
         //$('#portfolio').innerfade({ speed: 'slow', timeout: 4000, type: 'sequence', containerheight: '220px' }); 
         //$('.fade').innerfade({ speed: 'slow', timeout: 1000, type: 'sequence', containerheight: '1.5em' }); 
 
