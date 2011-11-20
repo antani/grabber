f4f522703bdc801585ae8385cff83b9da8fd4f08
Ved <ved.antani@gmail.com>
2011-11-20 19:38:19 +0530

    added cap task

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,3 +64,8 @@ namespace :bundler do
 end
  
 after 'deploy:update_code', 'bundler:bundle_new_release'
+
+after "deploy", "refresh_sitemaps"
+task :refresh_sitemaps do
+  run "cd #{latest_release} && RAILS_ENV=#{rails_env} rake sitemap:refresh"
+end
