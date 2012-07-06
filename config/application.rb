4159fc05763e11bca5fe0114125538fe0bd19b3c
Ved Antani <ved.antani@gmail.com>
2012-07-06 22:48:12 +0530

    precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -58,12 +58,12 @@ module Isbnnetin
     # Caching
     config.cache_store = :dalli_store, { :namespace => 'isbnnetin' }
     
-    config.gem "jammit"
+    #config.gem "jammit"
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.assets.precompile += %w( *.js *.css )
   end
 end
