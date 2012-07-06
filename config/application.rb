a9c5a9fc486aff4c09b9e6c7e057038ca2d7ca34
Ved Antani <ved.antani@gmail.com>
2012-07-07 01:05:17 +0530

    assets

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,6 @@ module Isbnnetin
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.precompile += %w( *.js *.css )
+    config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
   end
 end
